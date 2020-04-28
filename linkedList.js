6b77ace42e17301889a71e1714a4222d15bf228b
sunhui <2923132761@qq.com>
2020-04-28 14:07:42 +0800

    feat:singlelist remove

--- linkedList.js
+++ linkedList.js
@@ -1,6 +1,8 @@
 
 /**
  * @mark 单链表
+ * 
+ * 
  */
 class Node {
   constructor (element) {
@@ -33,8 +35,21 @@ class linkedList {
 
     currNode.next = newNode
   }
+
+  remove(element) {
+    let removeNode = this.find(element) // remove node
+
+    let currNode = this.header
+
+    while(currNode.next !== removeNode) {
+      currNode = currNode.next
+    }
+
+    currNode.next = removeNode.next
+  }
 }
 
 let singleLink = new linkedList()
 singleLink.insert('1', 'header')
 singleLink.insert('2', 'header') // header -> 2 -> 1
+singleLink.remove('2')  // header -> 1
