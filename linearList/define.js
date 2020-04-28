5a61bb614cfe1af6d089b9e0b7e07b8775e5009a
sunhui <2923132761@qq.com>
2020-04-29 00:32:47 +0800

    feat:lineList by server

--- linearList/define.js
+++ linearList/define.js
@@ -27,27 +27,27 @@
     }
 
     // 建立一个空的线性表
-    initList (list) {
+    initList () {
 
     }
 
     // 线性表为空返回true，否则返回false
-    listEmpty (list) {
+    listEmpty () {
 
     }
 
     // 将线性表清空
-    clear (list) {
+    clear () {
 
     }
 
     // 将线性表中第i个位置元素值返回给e
-    getElem (list, i, e) {
+    getElem (i) {
 
     }
 
     // 查找与e相等的元素，成功返回序号，失败返回0
-    locateElem (list, e) {
+    locateElem (e) {
 
     }
 
@@ -67,5 +67,87 @@
     }
   }
 
+  /**
+   * @mark 对象实现
+   * 问题：listInsert 对象插入number会自动转化为字符串/兼容掉了
+   * 数组实现类似做各种数组操作 不单独实现了
+   */
+  class lineList {
+    constructor () {
+      this.list = {}
+    }
 
+    // 建立一个空的线性表
+    initList () {
+      this.list = {}
+    }
+
+    // 线性表为空返回true，否则返回false
+    listEmpty () {
+      return this.listLength() === 0 ? true : false
+    }
+
+    // 将线性表清空
+    clear () {
+      this.list = {}
+    }
+
+    // 将线性表中第i个位置元素值返回给e
+    getElem (i) {
+      let e = null
+
+      Object.keys(this.list).map(key => {
+        key = typeof i === 'number' ? Number(key) : key
+        if (key === i) {
+          e = this.list[i]
+        }
+      })
+
+      return e
+    }
+
+    // 查找与e相等的元素，成功返回序号，失败返回0
+    locateElem (e) {
+      let indexList = []
+
+      Object.keys(this.list).map(key => {
+        if (this.list[key] === e) {
+          indexList.push(key)
+        }
+      })
+
+      return indexList || indexList.length
+    }
+
+    // 第i个位置插入新元素e
+    listInsert(i, e) {
+      this.list[i] = e
+    }
+
+    // 删除第i个位置元素，并用e返回其值
+    listDelete(i) {
+      let e = this.list[String(i)] // String 兼容 null undefined
+      delete this.list[String(i)]
+      return e
+    }
+
+    // 返回元素个数
+    listLength() {
+      return Object.keys(this.list).length
+    }
+  }
+
+  let classmates = new lineList()
+  classmates.listInsert(1, 'liming')
+  classmates.listInsert(2, 'hanmeimei')
+  classmates.listInsert(4, 'hanmeimei')
+
+  // list: {1: "liming", 2: "hanmeimei", 4: "hanmeimei"}
+  classmates.getElem(2) // "hanmeimei"
+  classmates.listLength() // 3
+  classmates.listEmpty() // false
+  classmates.locateElem('hanmeimei') // ["2", "4"]
+  classmates.listDelete(2) // "hanmeimei" classmates: list: {1: "liming", 4: "hanmeimei"}
+
+  classmates.clear() // classmates: list: {}
 
